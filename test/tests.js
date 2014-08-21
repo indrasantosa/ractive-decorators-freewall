6bb945b16928ac0b9d6e1131cb7ec7d23de75e7d
Indra Santosa <indrasantosa90@gmail.com>
2014-08-21 11:47:09 +0800

    Initial commit

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+// ractive-decorators-freewall tests
+// =============================================
+
+(function () {
+
+	var fixture;
+
+	test( 'We are ready to write some tests', function ( t ) {
+		t.ok( true );
+	});
+
+
+}());
